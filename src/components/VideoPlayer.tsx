54d4a7a2daa53d198a644ed2866e8fe24a3552cd
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2023-08-30 11:02:49 +0530

    temp1

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -13,6 +13,7 @@ const VideoPlayer = ({ videoUrl }: Props) => {
         sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation"
         allowFullScreen
       ></iframe> */}
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/B4Y9Ed4lLAI?si=E7UZ38ECCsUq4cfq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   )
 }
