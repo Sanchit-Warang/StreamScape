686d732752853b83312e4b675d015020cdb24616
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2023-09-10 14:53:35 +0530

    new logic to block ads

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,16 +1,32 @@
+import { useEffect } from 'react'
+
 type Props = {
   videoUrl: string
 }
 
-const VideoPlayer = ({videoUrl}: Props) => {
+const VideoPlayer = ({ videoUrl }: Props) => {
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      // Block pop-up windows
+      event.preventDefault()
+      event.returnValue = ''
+    }
+
+    window.addEventListener('beforeunload', handleBeforeUnload)
+
+    return () => {
+      // Clean up the event listener when the component unmounts
+      window.removeEventListener('beforeunload', handleBeforeUnload)
+    }
+  }, [])
   return (
-      <iframe
-        src={videoUrl}
-        title="Video Player"
-        // sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation"
-        allowFullScreen
-        className="w-full h-[80vh]"
-      ></iframe>
+    <iframe
+      src={videoUrl}
+      title="Video Player"
+      // sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation"
+      allowFullScreen
+      className="w-full h-[80vh]"
+    ></iframe>
   )
 }
 
